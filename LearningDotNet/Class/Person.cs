a887bf4477a7bfea0e9b7f2ee83d4f0537ae1bcc
Murillo Alves <tumusx1@gmail.com>
2023-09-09 12:16:04 -0300

    Add project files.

---
+++ LearningDotNet/Class/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Person
+{
+    int Id { get; set; }
+    int idadePessoa {  get; set;}
+    String nomePessoa { get; set;}
+    public Person(int idadePessoa, string nomePessoa)    {
+        this.nomePessoa = nomePessoa;
+        this.idadePessoa = idadePessoa;
+    }
+}
+
+class AccountPerson
+{
+    String addressAccount { get; set; }
+    private List<Person> persons { get; } = new List<Person>();
+    int idAccountCurrent { get; set; }
+
+    Person person { get; set;}
+
+    AccountTypes typeAccount;
+
+    public AccountPerson(String addressAccount, Person person, AccountTypes accountTypes)
+    {
+        this.addressAccount = addressAccount;
+        this.person = person;
+        this.typeAccount = accountTypes;
+    }
+}
+
+
+enum AccountTypes
+{
+    CONTA_CORRENTE, CONTA_SALARIO, CONTA_POUPANCA;
+}
---
+++ LearningDotNet/Class/Service/AccountPersonServices.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class AccountPersonServices : IAccountPersonServices
+{
+    public List<AccountPerson> GetAllAccounts()
+    {
+        List<AccountPerson> ListPersons = new List<AccountPerson>();
+        return ListPersons;
+    }
+
+    bool IAccountPersonServices.CreateNewAccountPerson(AccountPerson acccountPerson)
+    {
+        return true; 
+    }
+}
+
+interface IAccountPersonServices
+{
+    bool CreateNewAccountPerson(AccountPerson acccountPerson);
+    List<AccountPerson> GetAllAccounts();
+}
